894d265bd66cfb97808bfdd06ccaa352a18a45b9
Joe <62161622+jh508@users.noreply.github.com>
2023-09-12 15:35:45 +0100

    Create Two-Sum.java

---
+++ Two-Sum.java
@@ -0,0 +1,17 @@
+// Problem: Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
+            }
+            numMap.put(nums[i], i);
+        }
+
+        return new int[] {0, 1};
+    }
+}
